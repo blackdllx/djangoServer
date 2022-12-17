25842a002ab31019327c0eb9bdc7880a289b40a3
blackdllx <danylo.wowk@gmail.com>
2022-12-17 21:31:43 +0200

    s

--- WebServer/WebServer/urls.py
+++ WebServer/WebServer/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("main/", include("startPage.urls")),
+    path("", include("startPage.urls")),
     path('admin/', admin.site.urls),
 ]
